b6f389c4ead798fba1df7f1f4e3eca0e577a565c
Vitnere <nemanjakolar@gmail.com>
2016-11-26 14:55:16 +0100

    0.2 made admin, index and manage_content

---
+++ public/admin.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+    <h1>Widget Corp</h1>
+</div>
+<div id="main">
+    <div id="navigation">
+        &nbsp;
+    </div>
+    <div id="page">
+        <h2>Admin Menu</h2>
+        <p>Welcome to the admin area.</p>
+        <ul>
+            <li><a href="manage_content.php">Manage Website Content</a></li>
+            <li><a href="manage_admins.php">Manage Admin Users</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+</div>
+<div id="footer">Copyright 2016, Vitnere</div>
+</body>
+</html>
