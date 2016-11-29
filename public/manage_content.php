55ddc0457d9a886dc5a9526282f1d0131a8b077d
Vitnere <nemanjakolar@gmail.com>
2016-11-29 13:37:30 +0100

    0.9 Highlight selected navigation

--- public/manage_content.php
+++ public/manage_content.php
@@ -3,16 +3,16 @@
 <?php include("../includes/layouts/header.php"); ?>
 
 <?php
-    if(isset($_GET["subject"])){
-        $selected_subject_id = $_GET["subject"];
-        $selected_page_id = null;
-    }elseif(isset($_GET["page"])){
-        $selected_subject_id = null;
-        $selected_page_id = $_GET["page"];
-    }else{
-        $selected_subject_id = null;
-        $selected_page_id = null;
-    }
+if (isset($_GET["subject"])) {
+    $selected_subject_id=$_GET["subject"];
+    $selected_page_id=null;
+} elseif (isset($_GET["page"])) {
+    $selected_subject_id=null;
+    $selected_page_id=$_GET["page"];
+} else {
+    $selected_subject_id=null;
+    $selected_page_id=null;
+}
 ?>
 <div id="main">
     <div id="navigation">
@@ -21,21 +21,36 @@
             <?php
             while ($subject=mysqli_fetch_assoc($subject_set)) {
                 ?>
-                <li>
-                    <a href="manage_content.php?subject=<?php echo urlencode($subject["id"])?>"><?php echo $subject["menu_name"]; ?></a>
-                    <?php $page_set=find_pages_for_subject($subject["id"]); ?>
-                    <ul class="pages">
-                        <?php
-                        while ($page=mysqli_fetch_assoc($page_set)) {
-                            ?>
-                            <li>
-                                <a href="manage_content.php?subject=<?php echo urlencode($subject["id"])?>"><?php echo $page["menu_name"]; ?></a>
-                            </li>
-                            <?php
-                        }
+
+                <?php
+                    echo "<li";
+                    if ($subject["id"] == $selected_subject_id) {
+                        echo " class=\"selected\"";
+                    }
+                    echo ">";
+                ?>
+                <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
+                <?php $page_set=find_pages_for_subject($subject["id"]); ?>
+
+                <ul class="pages">
+                    <?php
+                    while ($page=mysqli_fetch_assoc($page_set)) {
                         ?>
-                        <?php mysqli_free_result($page_set); ?>
-                    </ul>
+
+                        <?php
+                            echo "<li";
+                            if ($page["id"] == $selected_page_id) {
+                                echo " class=\"selected\"";
+                            }
+                            echo ">";
+                        ?>
+                        <a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    <?php mysqli_free_result($page_set); ?>
+                </ul>
                 </li>
                 <?php
             }
