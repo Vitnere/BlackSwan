28d31dabeea43787d7153d5f5d5e1afcdac931d7
Vitnere <nemanjakolar@gmail.com>
2016-12-15 14:01:58 +0100

    2.2. Public page

--- public/index.php
+++ public/index.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php include("../includes/layouts/header.php"); ?>
 
-<html lang="en">
-	<head>
-		<title>Variables</title>
-	</head>
-	<body>
+<?php find_selected_page(); ?>
 
-	</body>
-</html>
+
+<div id="main">
+    <div id="navigation">
+        <?php echo public_navigation($current_subject, $current_page); ?>
+    </div>
+
+    <div id="page">
+        <?php echo message(); ?>
+        <?php if (isset($current_subject)) { ?>
+            <h2>Manage subjects</h2>
+            <p>Menu name: <?php echo htmlentities($current_subject["menu_name"]); ?><br>
+
+        <?php } elseif (isset($current_page)) { ?>
+            </p>
+            <h2>Manage pages</h2>
+            <p>Menu name: <?php echo htmlentities($current_page["menu_name"]); ?><br>
+
+            <div class="view-content">
+                <?php echo htmlentities($current_page["content"]); ?><br>
+            </div>
+
+        <?php } else {
+            echo("Please select subject or page");
+        }
+        ?>
+        </p>
+
+    </div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
